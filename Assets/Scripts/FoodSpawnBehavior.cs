064372825d4b95cc44f0ac81c7921fa1f45cbdc2
Mike B <berger.michael615@gmail.com>
2019-07-04 12:28:54 -0700

    Progress from 6/10 - 7/3

---
+++ Assets/Scripts/FoodSpawnBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawnBehavior : MonoBehaviour
+{
+    System.Random randGen;
+    Vector2 spawnDir;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randGen = new System.Random();
+        spawnDir = new Vector2(transform.position.x * (randGen.Next(-5, 5)), transform.position.y * (randGen.Next(-5, 5)));
+        speed = (float)randGen.Next(5,10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed > 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, spawnDir, speed * Time.deltaTime);
+            speed -= (float)0.2;
+        }
+    }
+}
---
+++ Assets/Scripts/FridgeBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckPlayCollision()
+    {
+        // if the player is colliding with the fridge while holding space, tie the fridge's movement with the player's
+        
+    }
+}
