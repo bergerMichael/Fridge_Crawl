737cc48e1b436a2c25b7142f3db15a77993c510d
Mike B <berger.michael615@gmail.com>
2019-12-30 14:02:16 -0800

    Added basic chest open/close animation w/ sfx.
    Next step will be to move food from inventory to chest where it will be deleted, a score counter incremented and the chest_close animation will be delayed until all food is deposited.

---
+++ Assets/Scripts/ChestScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour
+{
+    public AudioSource Chest_close;
+    public AudioSource Chest_open;
+    private bool Is_closed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Is_closed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            OperateChest();
+    }
+
+    void OperateChest()
+    {
+        if (Is_closed)
+        {
+            Is_closed = false;
+            GetComponent<Animator>().SetBool("Chest_Close", false);
+            Chest_open.Play();           
+        }
+        else
+        {
+            Is_closed = true;
+            GetComponent<Animator>().SetBool("Chest_Close", true);
+            Chest_close.Play();
+        }
+    }
+}
